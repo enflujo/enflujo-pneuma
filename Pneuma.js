7957bfc1eec05aa06cb16be87406510c72e47385
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-09-23 18:14:27 -0500

    🚀 Inicio

---
+++ Pneuma.js
@@ -0,0 +1,38 @@
+export default class Pneuma {
+  constructor() {
+    const ContextoAudio = window.AudioContext || window.webkitAudioContext;
+    this.ctx = new ContextoAudio();
+  }
+
+  crearOscilador() {
+    return this.ctx.createOscillator();
+  }
+
+  crearFuenteConArchivo(buffer) {
+    const fuente = new AudioBufferSourceNode(this.ctx);
+    fuente.buffer = buffer;
+    fuente.connect(this.ctx.destination);
+    return fuente;
+  }
+
+  crearAnalizador(fuente) {
+    const analizador = this.ctx.createAnalyser();
+    analizador.fftSize = 2048;
+    this.tamañoBuffer = analizador.frequencyBinCount;
+    this.datosAnalizador = new Uint8Array(this.tamañoBuffer);
+    analizador.getByteTimeDomainData(this.datosAnalizador);
+
+    fuente.connect(analizador);
+    return analizador;
+  }
+
+  async cargarAudio(url) {
+    const audioCrudo = await fetch(url).then((respuesta) => respuesta.arrayBuffer());
+    const audioDecodificado = await this.ctx.decodeAudioData(audioCrudo);
+    return this.crearFuenteConArchivo(audioDecodificado);
+  }
+
+  definirFrecuencia(fuente, frecuencia, tiempo = this.ctx.currentTime) {
+    fuente.frequency.setValueAtTime(frecuencia, tiempo);
+  }
+}
---
+++ espiritu.js
@@ -0,0 +1,77 @@
+import './estilos.scss';
+import Pneuma from './Pneuma';
+const soploRapido = 'soplo-rapido.wav';
+const pneuma = new Pneuma();
+
+const mensaje = document.getElementById('mensaje');
+const lienzo = document.getElementById('lienzo');
+const ctx = lienzo.getContext('2d');
+let audioCargado = false;
+let reproduciendo = false;
+let pasoX = 0;
+let analizador;
+let animar = false;
+
+const ancho = (lienzo.width = window.innerWidth);
+const alto = (lienzo.height = window.innerHeight);
+ctx.fillStyle = '#231942';
+ctx.lineWidth = 2;
+ctx.strokeStyle = '#e0b1cb';
+ctx.fillRect(0, 0, ancho, alto);
+
+async function inicio() {
+  const fuente = await pneuma.cargarAudio(soploRapido);
+  analizador = pneuma.crearAnalizador(fuente);
+
+  pasoX = ancho / pneuma.tamañoBuffer;
+  console.log(pasoX);
+  mensaje.innerText = 'Audio Cargado';
+
+  document.body.onclick = () => {
+    fuente.onended = () => {
+      animar = false;
+    };
+    animar = true;
+    pintar();
+
+    fuente.start();
+  };
+}
+
+// const fuente = pneuma.crearOscilador();
+// "sine", "square", "sawtooth", "triangle" and "custom"
+// fuente.type = 'triangle';
+// pneuma.definirFrecuencia(fuente, 20);
+
+// fuente.connect(pneuma.ctx.destination);
+// fuente.start();
+inicio();
+
+function pintar() {
+  let x = 0;
+
+  ctx.fillRect(0, 0, ancho, alto);
+  ctx.beginPath();
+
+  analizador.getByteTimeDomainData(pneuma.datosAnalizador);
+  console.log(pneuma.datosAnalizador);
+  for (let i = 0; i < pneuma.tamañoBuffer; i++) {
+    const v = pneuma.datosAnalizador[i] / 128;
+    const y = (v * alto) / 2;
+
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
+
+    x += pasoX;
+  }
+
+  ctx.lineTo(ancho, alto / 2);
+  ctx.stroke();
+
+  if (animar) {
+    requestAnimationFrame(pintar);
+  }
+}
